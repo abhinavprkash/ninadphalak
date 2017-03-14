7d33fe33bd7f5a9a3fb4cdf9ce1621f17068430e
Ninad Phalak <ninad.phalak@nokia.com>
2017-03-14 18:46:09 -0500

    karger attempt one

---
+++ algorithms/src/kargerMinCut/KargerMinCut.java
@@ -0,0 +1,37 @@
+package kargerMinCut;
+
+import java.util.Random;
+
+public class KargerMinCut {
+/*	Karger Algorithm:
+	1)  Initialize contracted graph G as copy of original graph
+	2)  While there are more than 2 vertices.
+	      a) Pick a random edge (u, v) in the contracted graph.
+	      b) Merge (or contract) u and v into a single vertex (update 
+	         the contracted graph).
+	      c) Remove self-loops
+	3) Return cut represented by two vertices.
+*/
+	
+	public static int kargerMinCut(Graph graph){
+		while(getNumOfVertices(graph) > 2){
+			/* To pick a random edge, we will pick a random key i.e vertex and then iterate over the value and pick a random node
+			 * So our (vertex,node) will be the random edge selected
+			 */
+			
+		}
+		return 0;
+	}
+	
+	public Node[] getRandomEdge(Graph graph){
+		String[] keys= (String[])graph.adjMap.keySet().toArray();
+		Random rand = new Random();
+		Node n1 = keys[rand.nextInt(keys.length)];
+		
+	}
+	
+	public static int getNumOfVertices(Graph g){
+		//number of vertices = total number of keys in G
+		return g.adjMap.size();
+	}
+}
